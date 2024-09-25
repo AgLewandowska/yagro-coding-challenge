dc91fed2c5aa7e60ef3c6e4c8f69bee498c969cf
ag <sky.october.returns@gmail.com>
2024-09-25 14:08:05 +0100

    Write more tests

--- src/main/scala/challenge/Worker.scala
+++ src/main/scala/challenge/Worker.scala
@@ -3,9 +3,13 @@ package challenge
 class Worker(val items: Set[Item],
              val assemblyStage: Int) {
 
-  override def toString: String = (items.mkString + (if (assemblyStage > 0) assemblyStage else " ")).padTo(3, ' ')
+  override def toString: String =
+    (items.mkString + (if (assemblyStage > 0) assemblyStage else " ")).padTo(3, ' ')
+    
   def hasFinishedProduct: Boolean = items.contains(Item.P)
-  def acceptsItem(item: Item): Boolean = Item.components.contains(item) && items.size < 2 && !items.contains(item)
+  
+  def acceptsItem(item: Item): Boolean =
+    Item.components.contains(item) && items.size < 2 && !items.contains(item)
 
   def assembleProduct(): Worker =
     if (items == Item.components.toSet && assemblyStage < 3)
--- src/test/scala/challenge/WorkerSpec.scala
+++ src/test/scala/challenge/WorkerSpec.scala
@@ -4,5 +4,62 @@ import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should
 
 class WorkerSpec extends AnyFlatSpec with should.Matchers {
-  
+  "hasFinishedProduct" should "return true if worker is holding a product" in {
+    val worker = Worker(Set(Item.P), 0)
+
+    worker.hasFinishedProduct should be(true)
+  }
+  it should "return true if worker is holding a product and another item" in {
+    val worker = Worker(Set(Item.P, Item.A), 0)
+
+    worker.hasFinishedProduct should be(true)
+  }
+  it should "return false if worker is not holding anything" in {
+    val worker = Worker(Set(), 0)
+
+    worker.hasFinishedProduct should be(false)
+  }
+  it should "return false if worker is not holding a product" in {
+    val worker = Worker(Set(Item.A, Item.B), 0)
+
+    worker.hasFinishedProduct should be(false)
+  }
+
+  "acceptsItem" should "accept item A if it is empty" in {
+    val worker = Worker.empty
+
+    worker.acceptsItem(Item.A) should be(true)
+  }
+  it should "accept item A if it is holding item B" in {
+    val worker = Worker(Set(Item.B), 0)
+
+    worker.acceptsItem(Item.A) should be(true)
+  }
+  it should "accept item A if it is holding a product" in {
+    val worker = Worker(Set(Item.P), 0)
+
+    worker.acceptsItem(Item.A) should be(true)
+  }
+  it should "not accept item A if it is holding two items already" in {
+    val worker = Worker(Set(Item.A, Item.B), 0)
+
+    worker.acceptsItem(Item.A) should be(false)
+  }
+
+  "assembling a product" should "increment assembly stage if the worker is holding items A and B and the stage is less than 3" in {
+    val worker = Worker(Set(Item.A, Item.B), 0)
+
+    worker.assembleProduct().assemblyStage shouldBe 1
+  }
+  it should "assemble product if the worker is holding items A and B and the stage is 3" in {
+    val worker = Worker(Set(Item.A, Item.B), 3)
+
+    worker.assembleProduct().assemblyStage shouldBe 0
+    worker.assembleProduct().items shouldBe Set(Item.P)
+  }
+  it should "not increment assembly stage if the worker is not holding items A and B" in {
+    val worker = Worker(Set(Item.A), 0)
+
+    worker.assembleProduct().assemblyStage shouldBe 0
+  }
 }
